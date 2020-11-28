eb97ee9cc74afc8f0453850fee7afbab5f8fcf8a
Ibram Darwish <ibramdarwish@Ibrams-MacBook-Pro.local>
2020-11-27 20:15:33 -0500

    add new db

---
+++ empolyees.sql
@@ -0,0 +1,79 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+
+-- Create the department table
+CREATE TABLE department(
+id int NOT NULL AUTO_INCREMENT,
+name varchar(30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+
+-- Create the role table
+CREATE TABLE role(
+id int NOT NULL AUTO_INCREMENT,
+title varchar(30) NOT NULL,
+salary decimal,
+department_id Int,
+PRIMARY KEY (id)
+);
+
+
+-- Create the Employee table.
+CREATE TABLE empolyee(
+id int NOT NULL AUTO_INCREMENT,
+first_name varchar(30) NOT NULL,
+last_name VARCHAR (30) NOT NULL,
+role_id int,
+PRIMARY KEY (id)
+);
+
+
+-- Inset data (empolyee table).
+INSERT INTO empolyee (first_name , last_name, role_id) VALUES ("Ibram","Elias", 1);
+INSERT INTO empolyee (first_name , last_name, role_id) VALUES ("Mina","Makram",2);
+INSERT INTO empolyee (first_name , last_name, role_id) VALUES ("Joe","Habib",3);
+
+
+-- Inset data (role table).
+INSERT INTO role (title , salary, department_id) VALUES ("accountant","8000",1);
+INSERT INTO role (title , salary, department_id) VALUES ("developer","12000",2);
+INSERT INTO role (title , salary, department_id) VALUES ("desginer","15000",3);
+
+
+-- Inset data (department table).
+INSERT INTO department (name) VALUES ("finance");
+INSERT INTO department (name) VALUES ("IT");
+INSERT INTO department (name) VALUES ("design");
+
+
+SELECT * from department;
+SELECT * from role;
+SELECT * from empolyee;
+
+select empolyee.first_name, empolyee.last_name, role.title, role.salary, department.name from employee 
+INNER join role  on empolyee.role_id = role.id
+INNER join department  on r.department_id = department.id
+order by department.name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const table = require('console.table');
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -7,11 +9,11 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "password",
-  database: "employee_tracker_DB"
+  database: "employees_DB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
@@ -21,21 +23,245 @@ connection.connect(function(err) {
 function start() {
   inquirer
     .prompt({
-      name: "addOrviewOrupdate",
+      name: "UserOptions",
       type: "list",
-      message: "What would you like to do in Departments, Roles and Employees?",
+      message: "What would you like to do ?",
       choices: ["View", "Add", "Update"]
     })
-    .then(function(answer) {
+    .then(function (answer) {
       // based on their answer, either call the bid or the post functions
-      if (answer.addOrviewOrupdate === "VIEW") {
-        viewDepartment();
+      if (answer.UserOptions === "View") {
+        view();
+      } else if (answer.UserOptions === "Add") {
+        add();
+      } else if (answer.UserOptions === "Update") {
+        update();
       }
-      else if(answer.addOrviewOrupdate === "ADD") {
+      else {
+        connection.end();
+      }
+    });
+}
+
+function view() {
+  inquirer
+    .prompt({
+      name: "Views",
+      type: "list",
+      message: "What would you like to see ?",
+      choices: ["Department", "Empolyees", "Roles", "All"]
+    })
+  .then(function (answer) {
+      // based on their answer, either call the bid or the post functions
+      switch (answer.Views) {
+        case "Department":
+          con.query("SELECT * FROM department", function (err, results) {
+            if (err) throw err;
+            // once you have the items, prompt the user for which they'd like to bid on
+            else {
+              console.table(results)
+              start();
+            }
+          });
+          break;
+        case "Empolyees":
+          con.query("SELECT * FROM empolyee", function (err, results) {
+            if (err) throw err;
+            // once you have the items, prompt the user for which they'd like to bid on
+            else {
+              console.table(results)
+              start();
+            }
+          });
+          break;
+        case "Roles":
+          con.query("SELECT * FROM role", function (err, results) {
+            if (err) throw err;
+            // once you have the items, prompt the user for which they'd like to bid on
+            else {
+              console.table(results)
+              start();
+            }
+          });
+          break;
+        case "All":
+          con.query("", function (err, results) {
+            if (err) throw err;
+            // once you have the items, prompt the user for which they'd like to bid on
+            else {
+              console.table(results)
+              start();
+            }
+          });
+          break;
+      }
+    });
+}
+
+
+
+
+
+
+
+
+
+// function viewDepartment() {
+//   // query the database for all items being auctioned
+
+// }
+// function viewEmpolyee() {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM empolyee", function (err, results) {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     else {
+//       console.table(results)
+//       start();
+//     }
+//   });
+// }
+// function viewRole() {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM role", function (err, results) {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     else {
+//       console.table(results)
+//       start();
+//     }
+//   });
+// }
+// function viewAll() {
+//   // query the database for all items being auctioned
+//   connection.query("", function (err, results) {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     else {
+//       console.table(results)
+//       start();
+//     }
+//   });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function viewDepartment() {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM department", function (err, results) {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     else {
+//       console.table(results)
+//       start();
+//     }
+//   });
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /////////////////////////////////////////////////////////////////////////////
+function viewRole() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM role", function (err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    else {
+      console.table(results)
+      start();
+    }
+  });
+};
+function viewEmpolyee() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM empolyee", function (err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    else {
+      console.table(results)
+      start();
+    }
+  });
+};
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "What is employee's first name ?"
+      },
+      {
+        type: "input",
+        name: "What is employee's last name ?"
+      },
+      {
+
+        type: "input",
+        name: "What is empolyee's department ?"
+      },
+      {
+
+        type: "input",
+        name: "What is empolyee's role ?"
+      },
+    ])
+    .then(function (answer) {
+      // based on their answer, either call the bid or the post functions
+      if (table.answer === "department") {
         addDepartment();
-      } else if (answer.addOrviewOrupdate === "UPDATE"){
-          updateDepartment();
-    }else{
+      }
+      else if (answer.answe === "role") {
+        addRole();
+      } else if (answer.answe === "empolyee") {
+        updateEmpolyee();
+      } else {
         connection.end();
       }
     });
